40ec536ddab9629c45968038535dd0efaecdc476
Kacper Beirowski <kacper.beirowski@gmail.com>
2024-08-03 16:03:37 +0200

    ADD: .env config for email, share post by email

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from django.core.mail import send_mail
 
 from .models import Post
+from .forms import EmailPostForm
 
 
 def post_list(request):
@@ -30,9 +32,28 @@ def post_detail(request, year, month, day, post):
 
     return render(request, 'blog/post/detail.html', {'post': post})
 
+
 class PostListView(ListView):
     """Alternative method to display list of posts"""
     queryset = Post.published.all()
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you {post.title}"
+            message = f"Read post on: {post_url}\n\n Comments: {cd['name']}: {cd['comments']}"
+            send_mail(subject, message, 'dev@kelek.kartuzy.pl', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
