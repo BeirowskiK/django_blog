42a22c37fedf2418a0a5adf9696d192cab05d9d0
Kacper Beirowski <kacper.beirowski@gmail.com>
2024-08-03 12:16:14 +0200

    ADD: Alternative class view for post list

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 from .models import Post
 
@@ -28,3 +29,10 @@ def post_detail(request, year, month, day, post):
     )
 
     return render(request, 'blog/post/detail.html', {'post': post})
+
+class PostListView(ListView):
+    """Alternative method to display list of posts"""
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
